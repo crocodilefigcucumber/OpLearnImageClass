2e007b903c6666f82810a8534d3f605b7f39e5bc
Michał Mikuta <mmikuta@ethz.ch>
2024-06-06 15:26:18 +0200

    Cleaned up

--- experiments/run.sh
+++ experiments/run.sh
@@ -1,2 +1,3 @@
-#python3 main.py --multirun +model=spectral model.spectral.ksize=[3,3], [5,5],[10,10],[15,15],[20,20],[25,25], [28,28]
-python3 main.py --multirun +model=spectral model.ksize=[3,3],[5,5],[10,10],[15,15],[20,20],[25,25],[28,28] train.opt.lr=0.005
+#python3 main.py --multirun +model=spectral model.ksize=[3,3],[5,5],[10,10],[15,15],[20,20],[25,25],[28,28] train.opt.lr=0.005
+
+
